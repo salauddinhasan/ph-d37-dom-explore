5b7704fe13cedb3ba6e2386a4b0ecf61f839e8d1
salauddin <salauddin@hasan.com>
2026-02-14 11:58:40 +0600

    DOM styling: querySelectorAll with loop

--- dom.js
+++ dom.js
@@ -1,1 +1,11 @@
-console.log('i have a plane');
+// const sections = document.getElementsByTagName('section');
+const sections = document.querySelectorAll('section')
+for(section of sections) {
+    console.log(section);
+    section.style.backgroundColor = 'lightblue';
+    section.style.border = '3px solid green';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.padding = '10px';
+
+}
